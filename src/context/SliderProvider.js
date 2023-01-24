128551b4f412fe3e2b8d0db4309aa0a68ddf2ec8
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 19:47:32 +0400

    added sliders data

--- src/context/SliderProvider.js
+++ src/context/SliderProvider.js
@@ -2,6 +2,61 @@ import { createContext,useContext,useState } from "react";
 
 export const SliderContext = createContext()
 
+
+const sliders = [
+    {
+      url:'https://images.unsplash.com/photo-1591534180437-507029f6ee60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80',
+      'az-AZ':{
+        desc:'Birinci Aciqlama'
+      },
+      'ru-RU':{
+        desc:'Первое описание'
+      },
+      'en-US':{
+        desc:'First Description'
+      },
+    },
+    {
+      url:'https://images.unsplash.com/photo-1554830310-5b57379d04e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      'az-AZ':{
+        desc:'Ikinci Aciqlama'
+      },
+      'ru-RU':{
+        desc:'Второе описание'
+      },
+      'en-US':{
+        desc:'Second Description'
+      },
+    },
+    {
+      url:'https://images.unsplash.com/photo-1569000971870-7605f9bd8734?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      
+      'az-AZ':{
+        desc:'Ucuncu Aciqlama'
+      },
+      'ru-RU':{
+        desc:'Третье описание'
+      },
+      'en-US':{
+        desc:'Third Description'
+      },
+  
+    },
+    {
+      url:'https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80',
+      
+      'az-AZ':{
+        desc:'Dorduncu Aciqlama'
+      },
+      'ru-RU':{
+        desc:'Четвертое описание'
+      },
+      'en-US':{
+        desc:'Fourth Description'
+      },
+    }
+  ]
+
 export const SliderProvider = ({children})=>{
     const localLang = localStorage.getItem('lang')
     const defaultLanguage = localLang ? localLang : navigator.language  
@@ -11,7 +66,8 @@ export const SliderProvider = ({children})=>{
         localLang,
         defaultLanguage,
         language,
-        setLanguage
+        setLanguage,
+        sliders
     }
     return <SliderContext.Provider value={values}>{children}</SliderContext.Provider>
 }
