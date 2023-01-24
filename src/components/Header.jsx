93b08cfd051615d8e9b9d2aec0ae37cbf1de9792
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 17:54:38 +0400

    gettin default language from localStorage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 import styles from './styles.module.css'
+import { useState,useEffect } from 'react'
 
 function Header() {
+
+  const localLang = localStorage.getItem('lang')
+  const defaultLanguage = localLang ? localLang : navigator.language
+  const [language,setLanguage] = useState(defaultLanguage)
+
+  useEffect(()=>{
+    localStorage.setItem('lang',language)
+  },[language])
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>React Language</div>
       <div className={styles.language}>
-        <select name="" id="">
-          <option value="">Azerbaycan</option>
-          <option value="">Russian</option>
-          <option value="">English</option>
+        <select value={language} name="language" id="language" onChange={(e)=>setLanguage(e.target.value)}>
+          <option value="az-AZ">Azerbaycan</option>
+          <option value="ru-RU">Russian</option>
+          <option value="en-US">English</option>
         </select>
       </div>
     </div>
