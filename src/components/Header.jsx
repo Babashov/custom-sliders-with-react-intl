6aeefbfcdb09a1a264b2100ab009d42491ee9f6e
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 17:18:34 +0400

    added logo & language html codes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import styles from './styles.module.css'
 
 function Header() {
   return (
-    <div>header</div>
+    <div className={styles.header}>
+      <div className={styles.logo}>React Language</div>
+      <div className={styles.language}>
+        <select name="" id="">
+          <option value="">Azerbaycan</option>
+          <option value="">Russian</option>
+          <option value="">English</option>
+        </select>
+      </div>
+    </div>
   )
 }
 
