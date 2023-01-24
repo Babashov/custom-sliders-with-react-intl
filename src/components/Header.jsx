0495463e771255d5fbbf13a34464caef0dc9913f
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 18:05:58 +0400

    added multi language

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
 import styles from './styles.module.css'
 import { useState,useEffect } from 'react'
+import {IntlProvider, FormattedMessage} from 'react-intl'
+
+const translationMsg = {
+  "az-AZ":{
+    title:'React Coxlu Dil'
+  },
+
+  "ru-RU":{
+    title:'React Язык'
+  },
+
+  "en-US":{
+    title:'React Language'
+  }
+}
+
 
 function Header() {
 
@@ -12,16 +28,20 @@ function Header() {
   },[language])
 
   return (
-    <div className={styles.header}>
-      <div className={styles.logo}>React Language</div>
-      <div className={styles.language}>
-        <select value={language} name="language" id="language" onChange={(e)=>setLanguage(e.target.value)}>
-          <option value="az-AZ">Azerbaycan</option>
-          <option value="ru-RU">Russian</option>
-          <option value="en-US">English</option>
-        </select>
+    <IntlProvider locale={language} messages={translationMsg[language]}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <FormattedMessage id='title'/>
+        </div>
+        <div className={styles.language}>
+          <select value={language} name="language" id="language" onChange={(e)=>setLanguage(e.target.value)}>
+            <option value="az-AZ">Azerbaycan</option>
+            <option value="ru-RU">Russian</option>
+            <option value="en-US">English</option>
+          </select>
+        </div>
       </div>
-    </div>
+    </IntlProvider>
   )
 }
 
